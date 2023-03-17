5c7cc4267742c603ec16f7999e83cd1a64d0250d
Yuya <takagif315@gmail.com>
2023-03-17 15:28:20 +0100

    wip

--- train.py
+++ train.py
@@ -109,8 +109,8 @@ def main(sess):
     trigger = util.trigger.Trigger(minimize=True)
     saver = tf.compat.v1.train.Saver(max_to_keep=None)
     num_train_iterations = train_dataloader.data_count // train_dataloader.batch_size # Iterations per training epoch.
-
     print('num_train_iterations', num_train_iterations)
+    print(train_dataloader.dataset)
     num_test_iterations = test_dataloader.data_count
     sess.run(tf.compat.v1.global_variables_initializer())
     
